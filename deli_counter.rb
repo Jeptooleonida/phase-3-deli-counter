feb4a6d12e62c6ff60cc2ec8763aaab2e73580be
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-08-22 04:17:05 +0300

    commit tree 4cbea345de85b185c65431cf288370470a74c1c6

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+katz_deli = ["New", "Day"]
+
+def line array
+    if array == []
+      puts "The line is currently empty."
+
+    else 
+        new_arr = array.map.with_index {|name, idx| "#{idx + 1}. #{name}"}
+        puts "The line is currently: #{new_arr.join(" ")}"
+
+    end
+end
+
+def take_a_number array, name
+    array = array << name
+    puts "Welcome, #{array[-1]}. You are number #{array.length} in line."
+  end
+
+  def now_serving array
+    if array == []
+      puts "There is nobody waiting to be served!"
+    else  
+      puts "Currently serving #{array[0]}."
+      array.shift()
+    end
+  end
